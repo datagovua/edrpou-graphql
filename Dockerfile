c34e7de7e34497e85c0345a3221a3f545213763e
vanuan <vanuan@gmail.com>
2016-05-15 13:42:27 +0300

    Initial Graphql implementation

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:4.4.3-slim
+
+RUN npm install npm@3 -g && npm install && npm run build-server
+
+WORKDIR /src
+
+CMD npm run production
+
+EXPOSE 80
---
+++ schema.js
@@ -0,0 +1,73 @@
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+} from 'graphql';
+
+import fetch from 'node-fetch';
+
+const BASE_URL = 'http://edr.data-gov-ua.org/api';
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  description: '...',
+  fields: () => ({
+    officialName: {type: GraphQLString},
+    name: {type: GraphQLString},
+    address: {type: GraphQLString},
+    edrpou: {type: GraphQLInt},
+    mainPerson: {type: GraphQLString},
+    occupation: {type: GraphQLString},
+    status: {type: GraphQLString}
+  })
+});
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Реєстр юридичних осіб України',
+
+  fields: () => ({
+    companies: {
+      type: new GraphQLList(CompanyType),
+      args: {
+        searchByEdrpou: { type: GraphQLInt },
+        searchByName: { type: GraphQLString },
+        first: { type: GraphQLInt },
+        after: { type: GraphQLInt }
+      },
+      resolve: (root, args) => {
+        let where = {};
+        let pagination = "";
+        if(args.searchByName) {
+          where.or = [{
+            officialName: { contains: encodeURIComponent(args.searchByName) }
+          }, {
+            name: { contains: encodeURIComponent(args.searchByName) }
+          }];
+        }
+        if(args.searchByEdrpou) {
+          let edrpou = edrpouToString(args.searchByEdrpou);
+          console.log(edrpou)
+          where.edrpou = encodeURIComponent(edrpou);
+        }
+        if(args.first != undefined && args.after != undefined) {
+          pagination = `&skip=${args.after}&limit=${args.first}`;
+        }
+        where = JSON.stringify(where);
+        return fetch(`${BASE_URL}/companies?where=${where}&sort=edrpou${pagination}`)
+        .then(res => res.json());
+      }
+    }
+  })
+});
+
+const edrpouToString = (id) => {
+  const pad = "00000000";
+  return (pad+id).slice(-pad.length);
+}
+
+export default new GraphQLSchema({
+  query: QueryType
+});
