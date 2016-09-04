5ab957065d243e86b117c8e2f589a775514e6d47
vanuan <vanuan@gmail.com>
2016-09-04 22:30:32 +0300

    Log all errors

--- server/index.js
+++ server/index.js
@@ -7,9 +7,21 @@ import schema from './schema';
 
 const app = express();
 
+var formatError => {
+  var err = {
+    message: error.message,
+    locations: error.locations,
+    stack: error.stack
+  }
+  console.log(JSON.stringify(err));
+  delete err.stack;
+  return err;
+}
+
 app.use('/', cors(), graphQLHTTP({
   schema,
-  graphiql: true,
+  graphiql: true
+  formatError
 }));
 
 app.listen(80);
