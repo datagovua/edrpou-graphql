e4f5100808b0bf325f58fd2cb45b07d4be8e3f7f
vanuan <vanuan@gmail.com>
2017-06-07 12:43:02 +0300

    rollbar, favicon fix, filter errors

--- server/index.js
+++ server/index.js
@@ -6,23 +6,69 @@ import Rollbar from 'rollbar';
 import schema from './schema';
 
 
-const rollbar = new Rollbar("0fe09289a0e44d2a93fead6605d23983");
+const rollbar = new Rollbar({
+  accessToken: "0fe09289a0e44d2a93fead6605d23983",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
 const app = express();
 
+const ignoredMessages = ["Must provide query string"];
+const shouldIgnore = (error) => {
+  try {
+    return ignoredMessages.map((ignored) => error.message.includes(ignored)).some(el => el === true);
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+const fillErrorName = (err) => {
+  let errorName;
+  try {
+    errorName = err.stack.split('\n')[0].split(/[:(]/)[0];
+  } catch(_) {}
+  if(errorName && !err.name) {
+    err.name = errorName;
+  }
+}
+
+const rollbarError = (err) => {
+  if(err) {
+    console.log('Error reporting to Rollbar', err);
+  }
+}
+
 const formatError = (error, req) => {
-  var err = {
+  let err = {
     message: error.message,
     locations: error.locations,
     stack: error.stack
   }
-  console.log(JSON.stringify(err));
-  rollbar.error("GraphQL error", err);
+
+  fillErrorName(err);
+  if(!shouldIgnore(err)) {
+    console.log(JSON.stringify(err));
+    if (error instanceof Error) {
+      return rollbar.error(err.name, err, req, rollbarError);
+    }
+    rollbar.error('GraphQL error: ' + err, req, rollbarError);
+  }
   delete err.stack;
   return err;
 };
 
-app.use('/', cors(), graphQLHTTP(req => ({
+
+const handleNonRoot = function (req, res, next) {
+  if(req.path === '/favicon.ico') {
+    res.status(404)
+   .send('Not found');
+  } else {
+    next();
+  }
+}
+
+app.use('/', handleNonRoot, cors(), graphQLHTTP(req => ({
   schema,
   graphiql: true,
   formatError: error => formatError(error, req)
