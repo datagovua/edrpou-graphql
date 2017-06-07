23c655270b257ba5c1e44054d2d8358fb80ea2a3
vanuan <vanuan@gmail.com>
2017-06-07 07:55:09 +0300

    Notify Rollbar errors

--- server/index.js
+++ server/index.js
@@ -1,27 +1,31 @@
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
 import cors from 'cors';
-
+import Rollbar from 'rollbar';
 
 import schema from './schema';
 
+
+const rollbar = new Rollbar("0fe09289a0e44d2a93fead6605d23983");
+
 const app = express();
 
-const formatError = (error) => {
+const formatError = (error, req) => {
   var err = {
     message: error.message,
     locations: error.locations,
     stack: error.stack
   }
   console.log(JSON.stringify(err));
+  rollbar.error("GraphQL error", err);
   delete err.stack;
   return err;
 };
 
-app.use('/', cors(), graphQLHTTP({
+app.use('/', cors(), graphQLHTTP(req => ({
   schema,
   graphiql: true,
-  formatError
-}));
+  formatError: error => formatError(error, req)
+})));
 
 app.listen(80);
